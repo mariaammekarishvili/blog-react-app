42f52d491c849deb230af6d87068841b8928e8f8
MariamMEK <65685526+mariaammekarishvili@users.noreply.github.com>
2022-12-22 21:52:56 +0400

    create post, edit and delete requests

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -20,3 +20,21 @@ export const createPost = async (title: string, body: string) => {
     console.error(err)
   }
 }
+
+export const editPost = async (post: any) => {
+  try {
+    const result = await axios.put(`${API}/posts/${post.id}`, post)
+    return result?.data 
+   }catch (err) {
+    console.error(err);
+  }
+}
+
+export const deletePost = async (postId: number) => {
+  try {
+    const result = await axios.put(`${API}/posts/${postId}`)
+    return result?.data 
+   }catch (err) {
+    console.error(err);
+  }
+}
