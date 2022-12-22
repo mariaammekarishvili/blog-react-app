e12d7b8ff7342bd913a53dddc366e9514b8607c4
MariamMEK <65685526+mariaammekarishvili@users.noreply.github.com>
2022-12-22 23:58:05 +0400

    fix names

--- src/assets/svg/LoaderIcon.tsx
+++ src/assets/svg/LoaderIcon.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 const LoaderIcon = (
   props: JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>
 ) => (
-    <svg
+  <svg
     width="24px"
     height="24px"
     viewBox="0 0 20 20"
